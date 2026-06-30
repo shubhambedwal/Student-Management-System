271a3392ce93b0dfce4e6338fcd26d390705d5f1
shubhambedwal <152150885+shubhambedwal@users.noreply.github.com>
2026-06-30 08:57:18 +0530

    UI.java
    UI file created

---
+++ UI.java
@@ -0,0 +1,24 @@
+======== SMART STUDENT ========
+
+1. Login
+
+Username:
+Password:
+
+Login Successful
+
+-----------------------
+
+1. Add Student
+
+2. View Students
+
+3. Update Student
+
+4. Delete Student
+
+5. Search Student
+
+6. Statistics
+
+7. Exit
